226284269118c727bb4fe384b042af06923a8702
HypershadiX <ja.astudillob@duocuc.cl>
2025-07-10 18:29:17 -0400

    Ejercicio Python agregado

---
+++ Repaso.py
@@ -0,0 +1,53 @@
+mayor = -1
+menor = 101
+
+while True:
+    print("***MENÚ PRINCIPAL***")
+    print("1. Ingresar número")
+    print("2. Mostrar mayor")
+    print("3. Mostrar menor")
+    print("4. Terminar programa")
+    
+    
+    opcion = input("Ingrese una opción: ")
+    if opcion == "1":
+        while True:
+            try:
+                numero = int(input("Ingrese un número entre el 0 y el 100: "))
+                if numero >= 0 and numero <= 100:
+                    print("Ingreso exitoso")
+                    break
+                else:
+                    print("Debe ingresar un número entre el 0 al 100")
+            except ValueError:
+                    print("Debe ingresar un número entero")
+        if numero > mayor:
+                 mayor = numero
+        elif numero < menor:
+                 menor = numero
+        
+    elif opcion == "2":
+        if mayor == -1:
+            print("No se han registrado números")
+        
+        else:
+            print("El número mayor actualmente es", mayor)
+    
+    elif opcion == "3":
+        if menor == 101:
+            print("No se han registrado números")
+        
+        else:
+            print("El número menor actualmente es:", menor)
+    
+    elif opcion == "4":
+        print("Fin del programa")
+        break
+        
+    else:
+        print("Debe ingresar una opción válida")
+            
+
+            
+            
+    
